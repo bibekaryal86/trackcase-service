387ad6152877f0cbb4c4e866ebf655aba66326c2
Bibek Aryal <bibekaryal86@gmail.com>
2023-09-17 08:31:48 -0700

    Basic CRUD, only R so far

---
+++ src/trackcase_service/db/crud.py
@@ -0,0 +1,23 @@
+from typing import TypeVar, Type, List
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+ModelType = TypeVar("ModelType")
+
+
+class BaseSchema(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+class CrudActions:
+    def __init__(self, db_session: Session, db_model: Type[ModelType]):
+        self.db_model = db_model
+        self.db_session = db_session
+
+    def get_by_id(self, model_id: int) -> BaseSchema:
+        return self.db_session.query(self.db_model).filter(self.db_model.id == model_id).first()
+
+    def get_all(self, skip: int = 0, limit: int = 1000) -> List[BaseSchema]:
+        records = self.db_session.query(self.db_model).offset(skip).limit(limit).all()
+        return records
