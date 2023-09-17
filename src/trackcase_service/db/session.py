b3550cb196514311b27d24c9c4a5550c40417397
Bibek Aryal <bibekaryal86@gmail.com>
2023-09-16 23:02:59 -0700

    Update session.py

--- src/trackcase_service/db/session.py
+++ src/trackcase_service/db/session.py
@@ -14,7 +14,7 @@ db2_url = (
     f"ibm_db_sa://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}"
     f".c3n41cmd0nqnrk39u98g.databases.appdomain.cloud:30604/bludb;SECURITY=SSL;PROTOCOL=TCPIP;"  # noqa: E501
 )
-engine = create_engine(db2_url, pool_pre_ping=True, echo=True)
+engine = create_engine(db2_url, echo=False)     # use echo=True to show log in SysOut
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
