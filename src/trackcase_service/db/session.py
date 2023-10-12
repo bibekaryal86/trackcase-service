314547b73adb7715bf4067f1b975cec72629e86d
Bibek Aryal <bibekaryal86@gmail.com>
2023-10-11 21:26:36 -0700

    Remove SQLAlchemy Logging

--- src/trackcase_service/db/session.py
+++ src/trackcase_service/db/session.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from src.trackcase_service.utils.constants import DB_PASSWORD, DB_USERNAME
 
 url = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@bubble.db.elephantsql.com/{DB_USERNAME}"
-engine = create_engine(url, echo=True)  # use echo=True to show log in SysOut
+engine = create_engine(url, echo=False)  # use echo=True to show log in SysOut
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
