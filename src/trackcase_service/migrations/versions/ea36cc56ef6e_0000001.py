f7358d8e8849eaaa73e608291c4360b30a4d956b
Bibek Aryal <bibekaryal86@gmail.com>
2024-01-29 22:13:26 -0700

    Form Model/Schema Changes

--- src/trackcase_service/migrations/versions/ea36cc56ef6e_0000001.py
+++ src/trackcase_service/migrations/versions/ea36cc56ef6e_0000001.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "ea36cc56ef6e"
+revision: str = "23b617e5adef"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
